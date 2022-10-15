48688a228d389105549b64a725c2eefe9ad576e8
Mariusz Różycki <mar.rozycki@gmail.com>
2022-10-15 20:20:55 +0200

    Print IP address after connecting to the network

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,10 @@ int main() {
         return 1;
     } else {
         printf("Connected.\n");
+
+        extern cyw43_t cyw43_state;
+        auto ip_addr = cyw43_state.netif[CYW43_ITF_STA].ip_addr.addr;
+        printf("IP Address: %lu.%lu.%lu.%lu\n", ip_addr & 0xFF, (ip_addr >> 8) & 0xFF, (ip_addr >> 16) & 0xFF, ip_addr >> 24);
     }
     
     run_server();
