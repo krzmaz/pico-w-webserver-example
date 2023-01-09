24a397594a9d8f1ab065e64a2f9ae0ee51d02836
Gerhard Schiller <gerhard.schiller@gmail.com>
2023-01-09 12:38:43 +0100

    Added cgi functionality

--- src/ssi.h
+++ src/ssi.h
@@ -9,10 +9,19 @@
 // max length of the tags defaults to be 8 chars
 // LWIP_HTTPD_MAX_TAG_NAME_LEN
 const char * __not_in_flash("httpd") ssi_example_tags[] = {
-  "Hello",
-  "counter",
-  "GPIO"
+	"Hello",		// 0
+	"counter",	// 1
+	"GPIO",		// 2
+	"state1",		// 3
+	"state2",		// 4
+	"state3",		// 5
+	"state4",		// 6
+	"bg1",		// 7
+	"bg2",		// 8
+	"bg3",		// 9
+	"bg4"		// 10
 };
+
 u16_t __time_critical_func(ssi_handler)(int iIndex, char *pcInsert, int iInsertLen) {
   size_t printed;
   switch (iIndex) {
@@ -32,6 +41,49 @@ u16_t __time_critical_func(ssi_handler)(int iIndex, char *pcInsert, int iInsertL
       printed = snprintf(pcInsert, iInsertLen, "%f", voltage);
     }
     break;
+  case 3: /* "state1" */
+  case 4: /* "state2" */
+  case 5: /* "state3" */
+  case 6: /* "state4" */
+  {
+	  bool state;
+	  if(iIndex == 3)
+		  state = gpio_get(LED1);
+	  else if(iIndex == 4)
+		  state = gpio_get(LED2);
+	  else if(iIndex == 5)
+		  state = gpio_get(LED3);
+	  else if(iIndex == 6)
+		  state = gpio_get(LED4);
+
+	  if(state)
+		  printed = snprintf(pcInsert, iInsertLen, "checked");
+	  else
+		  printed = snprintf(pcInsert, iInsertLen, " ");
+  }
+  break;
+
+  case 7:  /* "bg1" */
+  case 8:  /* "bg2" */
+  case 9:  /* "bg3" */
+  case 10: /* "bg4" */
+  {
+	  bool state;
+	  if(iIndex == 7)
+		  state = gpio_get(LED1);
+	  else if(iIndex == 8)
+		  state = gpio_get(LED2);
+	  else if(iIndex == 9)
+		  state = gpio_get(LED3);
+	  else if(iIndex == 10)
+		  state = gpio_get(LED4);
+
+	  if(state)
+		  printed = snprintf(pcInsert, iInsertLen, "\"background-color:green;\"");
+	  else
+		  printed = snprintf(pcInsert, iInsertLen, "\"background-color:red;\"");
+  }
+  break;
   default: /* unknown tag */
     printed = 0;
     break;
