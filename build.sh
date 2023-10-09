e760d054524acd239bacd126c91a0f4eb9388730
Krzysztof Mazur <krzmazur1@gmail.com>
2023-10-09 19:23:26 +0200

    Fix Windows CI by using ninja

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if which ninja >/dev/null; then
-    cmake -B build -G Ninja $1 && \
+    cmake -B build -G Ninja && \
     ninja -C build $1
 else
-    cmake -B build $1 && \
+    cmake -B build && \
     make -j $(getconf _NPROCESSORS_ONLN) -C build $1 && \
     echo "done. P.S.: Consider installing ninja - it's faster"
 fi
